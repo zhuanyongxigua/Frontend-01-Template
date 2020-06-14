09911334411d528f7824e963ccd6f5e3db6cf96d
yuxin <yx18513839223@gmail.com>
2020-06-14 23:32:18 +0800

    feat(week10): add TicTacToeWinter

---
+++ week10/browser-sync.config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yuxin on 2018/1/25.
+ */
+
+"use strict";
+var browserSync = require("browser-sync").create();
+
+browserSync.init({
+    files: ['./*.html'],
+    server: true,
+    startPath: "./TicTacToe.html",
+    localOnly: false,
+    // host: "192.168.199.120",
+    port: 8081,
+    notify: false,
+});
