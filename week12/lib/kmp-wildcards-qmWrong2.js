453d1a99724e083b9750f5cff7abd3d6c655f764
于鑫 <xin.yu@chaitin.com>
2020-07-02 16:14:59 +0800

    feat(week12): fail at cook tabke

--- week12/lib/kmp-wildcards-qmWrong2.js
+++ week12/lib/kmp-wildcards-qmWrong2.js
@@ -1,3 +1,12 @@
+// 直接造一个完美的table是不可能的
+// 因为ab??eabcda这样的模式串有不同的table，都是合理的
+// 比如000001234
+// 比如0001234343
+// 比如001201234
+// 比如000121234
+// 比如000012000 对应abeaeabcda
+// 不过应该可以想办法改进，比如第一种table的第一个1是所有的table最靠后的第一个1
+// 所以如果用一个table匹配失败了，可以试着把这个table的第一个1向后移动，然后再匹配
 function find(source, pattern) {
   let table = new Array(pattern.length).fill(0);
   let k = -1;
@@ -36,6 +45,5 @@ console.log(find('abcdeabcdeabcda', 'ab??eabcda'));
 // console.log(find('abcdeabcdeabcda', 'abcd??bcda'));
 // console.log(find('abcdeabcdeabcda', 'abcde??cda'));
 // console.log(find('xyab', 'ac'));
-  
-  module.exports = find
-  
+
+module.exports = find
