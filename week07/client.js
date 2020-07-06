4df7eb4ca7f1b2d4071c6274e746714d49028d2d
于鑫 <xin.yu@chaitin.com>
2020-07-06 10:57:53 +0800

    feat(week07): init layout

---
+++ week07/client.js
@@ -0,0 +1,224 @@
+const net = require('net');
+const parser = require('./parser');
+
+class TrunkedBodyParser {
+  constructor() {
+    this.WAITING_LENGTH = 0;
+    this.WAITING_LENGTH_LINE_END = 1;
+    this.READING_TRUNK = 2;
+    this.WAITING_NEW_LINE = 3;
+    this.WAITING_NEW_LINE_END = 4;
+    this.length = 0;
+    // 字符串加法运算性能较差
+    this.content = [];
+    this.isFinished = false;
+    this.current = this.WAITING_LENGTH;
+  }
+  receiveChar(char) {
+    if (this.isFinished === true) {
+      return;
+    }
+    if (this.current === this.WAITING_LENGTH) {
+      if (char === '\r') {
+        if (this.length === 0) {
+          this.isFinished = true;
+        }
+        this.current = this.WAITING_LENGTH_LINE_END;
+      } else {
+        // 这个地方没动，老说说的是在十进制的末尾加一位
+        // this.length *= 10
+        // length应该是16进制
+        this.length *= 16;
+        // 这里用Number(char)也是可以的
+        // this.length += char.charCodeAt(0) - '0'.charCodeAt(0)
+        this.length += parseInt(char, 16);
+      }
+    } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+      if (char === '\n') {
+        this.current = this.READING_TRUNK;
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      this.content.push(char);
+      this.length --;
+      if (this.length === 0) {
+        this.current = this.WAITING_NEW_LINE;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_NEW_LINE_END;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_LENGTH;
+      }
+    }
+  }
+}
+
+class ResponseParser {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_SPACE = 3;
+    this.WAITING_HEADER_VALUE = 4;
+    this.WAITING_HEADER_LINE_END = 5;
+    this.WAITING_HEADER_BLOCK_END = 6;
+    this.WAITING_BODY = 7;
+
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = '';
+    this.headers = {};
+    this.headerName = '';
+    this.headerValue = '';
+    this.bodyParser = null;
+  }
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished;
+  }
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
+  receive(string) {
+    this.string = string;
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+  }
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      } else {
+        this.statusLine += char;
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if (char === '\r') {
+        this.current = this.WAITING_HEADER_BLOCK_END;
+        // 在这之前无法得知header里面的encoding是什么类型
+        // 所以body要在这里开始
+        if (this.headers['Transfer-Encoding'] === 'chunked') {
+          this.bodyParser = new TrunkedBodyParser();
+        }
+      } else {
+        this.headerName += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    } else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = '';
+        this.headerValue = '';
+      } else {
+        this.headerValue += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+      // \n是一个回车
+      if (char === '\n') {
+        this.current = this.WAITING_BODY;
+      }
+    } else if (this.current === this.WAITING_BODY) {
+      this.bodyParser.receiveChar(char);
+    }
+  }
+}
+
+class Request {
+  constructor(options) {
+    this.method = options.method || "GET";
+    this.host = options.host;
+    this.port = options.port || 80;
+    this.path = options.path || '/';
+    this.body = options.body || {};
+    this.headers = options.headers || {}
+    if (!this.headers["Content-Type"]) {
+      this.headers["Content-Type"] = "application/x-www-form-urlencoded";
+    }
+    if (this.headers["Content-Type"] === "application/json") {
+      this.bodyText = JSON.stringify(this.body);
+    } else if (this.headers["Content-Type"] === "application/x-www-form-urlencoded") {
+      this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
+    }
+    this.headers["Content-Length"] = this.bodyText.length;
+  }
+
+  toString() {
+    return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r
+\r
+${this.bodyText}`;
+  }
+  send(connection) {
+    return new Promise((resolve, reject) => {
+      const parser = new ResponseParser;
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        connection = net.createConnection({
+          host: this.host,
+          port: this.port
+        }, () => {
+          connection.write(this.toString());
+        })
+      }
+      // 如果body很大，那可能是有多个data
+      connection.on('data', (data) => {
+        parser.receive(data.toString());
+        if (parser.isFinished) {
+          resolve(parser.response);
+        }
+        connection.end();
+      })
+      connection.on('error', (err) => {
+        reject(err);
+        connection.end();
+      })
+    })
+  }
+}
+
+void async function() {
+  let request = new Request({
+    method: "POST",
+    host: "127.0.0.1",
+    port: "8088",
+    path: "/",
+    headers: {
+      ["X-Foo2"]: "customed"
+    },
+    body: {
+      name: "winter"
+    }
+  });
+
+  // 正常这里应该做成generator的形式的，但是太麻烦，就简化了
+  let response = await request.send();
+  let dom = parser.parseHTML(response.body);
+  console.log(JSON.stringify(dom, null, '    '));
+}()
+
+class Response {
+
+}
---
+++ week07/parser.js
@@ -0,0 +1,395 @@
+const css = require('css');
+const layout = require('./layout');
+// 有EOF的原因是，字符可以是一段一段传过来的
+// 编译器不知道当前这一段是不是最后一段，所以可能就会处在一个等待字符补全的状态
+// 由于字符会占位，所以不能用string，但是用对象什么的是可以的，这个地方没明白
+const EOF = Symbol('EOF'); // End of File
+
+
+let currentToken = null;
+let currentAttribute = null;
+let currentTextNode = null;
+
+let stack = [{ type: 'document', children: []}];
+
+let rules = [];
+function addCSSRules(text) {
+  var ast = css.parse(text);
+  rules.push(...ast.stylesheet.rules);
+}
+
+function match(element, selector) {
+  if (!selector || !element.attributes) {
+    return false;
+  }
+  if (selector.charAt(0) === '#') {
+    // 这里不知道是不是这样写的
+    var attr = element.attributes.filter(attr => attr.name === 'id')[0]
+    if (attr && attr.value === selector.replace('#', '')) {
+      return true;
+    }
+  } else if(selector.charAt(0) === '.') {
+    var attr = element.attributes.filter(attr => attr.name === 'class')[0]
+    // 额外的作业，空格分割的class
+    if (attr && attr.value === selector.replace('.', '')) {
+      return true;
+    }
+  } else {
+    if (element.tagName === selector) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function specificity(selector) {
+  // 第一个是高位
+  var p = [0, 0, 0, 0];
+  var selectorParts = selector.split(' ');
+  for (const part of selectorParts) {
+    // 可以在这里加正则把复合选择器也拆开
+    if (part.charAt(0) === '#') {
+      p[1] += 1;
+    } else if (part.charAt(0) === '.') {
+      p[2] += 1;
+    } else {
+      p[3] += 1;
+    }
+  }
+  return p;
+}
+
+function compare(sp1, sp2) {
+  if (sp1[0] - sp2[0]) {
+    return sp1[0] - sp2[0];
+  }
+  if (sp1[1] - sp2[1]) {
+    return sp2[1] - sp2[1];
+  }
+  if (sp1[2] - sp2[2]) {
+    return sp1[2] - sp2[2];
+  }
+  return sp1[3] - sp2[3];
+}
+
+// Third step
+function computeCSS(element) {
+  var elements = stack.slice().reverse();
+  // Fourth step
+  if (!element.computedStyle) {
+    element.computedStyle = {};
+  }
+  for (const rule of rules) {
+    var selectorParts = rule.selectors[0].split(' ').reverse();
+    if (!match(element, selectorParts[0])) {
+      continue;
+    }
+    let matched = false;
+    var j = 1;
+    for (let i = 0; i < elements.length; i++) {
+      if (match(elements[i], selectorParts[j])) {
+        j++;
+      }
+    }
+    if (j >= selectorParts.length) {
+      matched = true;
+    }
+    if (matched) {
+      // 如果匹配到，我们要加入
+      // Sixth step
+      var sp = specificity(rule.selectors[0]);
+      var computedStyle = element.computedStyle;
+      // CSS选择器大括号里面的一行就是一个declaration
+      for (const declaration of rule.declarations) {
+        if (!computedStyle[declaration.property]) {
+          // 由于后面还要处理优先级，所以这里放一个对象，然后把declaration的value存到这个对象的属性里面
+          computedStyle[declaration.property] = {};
+        }
+        if (!computedStyle[declaration.property].specificity) {
+          computedStyle[declaration.property].value = declaration.value;
+          computedStyle[declaration.property].specificity = sp;
+        } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
+          computedStyle[declaration.property].value = declaration.value;
+          computedStyle[declaration.property].specificity = sp;
+        }
+      }
+    }
+  }
+}
+
+function emit(token) {
+  let top = stack[stack.length - 1];
+  if (token.type === 'startTag') {
+    let element = {
+      type: 'element',
+      children: [],
+      attributes: []
+    }
+    element.tagName = token.tagName;
+    for(let p in token) {
+      if (p !== 'type' && p !== 'tagName') {
+        element.attributes.push({
+          name: p,
+          value: token[p]
+        });
+      }
+    }
+    // 第二步
+    // 在创建元素并且把tagName和属性都加好之后
+    // 这个位置非常的关键，也是非常难想的地方
+    computeCSS(element);
+    top.children.push(element);
+    element.parent = top;
+    if (!token.isSelfClosing) {
+      stack.push(element);
+    }
+    currentTextNode = null;
+  } else if (token.type === 'endTag') {
+    if(top.tagName !== token.tagName) {
+      throw new Error("Tag start end doesn't match!");
+    } else {
+      // 第三节，遇到style标签时，执行添加CSS规则的操作，执行CSS收集
+      // 在这里做的原因时是在push的时候style标签的子元素，也就是文本节点
+      // 还没有被挂到style标签上，style标签里面子元素会是空的
+      if (top.tagName === 'style') {
+        addCSSRules(top.children[0].content);
+      }
+      layout(top);
+      stack.pop();
+    }
+    currentTextNode = null
+  } else if (token.type === 'text') {
+    if(currentTextNode === null) {
+      currentTextNode = {
+        type: 'text',
+        content: ''
+      }
+      top.children.push(currentTextNode);
+    }
+    currentTextNode.content += token.content;
+  }
+}
+
+function data(c) {
+  if(c === '<') {
+    return tagOpen;
+  } else if (c === EOF) {
+    emit({
+      type: 'EOF'
+    });
+    return;
+  } else {
+    emit({
+      type: 'text',
+      content: c
+    });
+    return data;
+  }
+}
+
+function tagOpen(c) {
+  if(c === '/') {
+    return endTagOpen;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: 'startTag',
+      tagName: ''
+    }
+    return tagName(c);
+  } else {
+    emit({
+      type: 'text',
+      content: c
+    });
+    return;
+  }
+}
+
+function endTagOpen(c) {
+  if(c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: 'endTag',
+      tagName: ''
+    }
+    return tagName(c);
+  } else if (c === '>') {
+
+  } else if (c === EOF) {
+
+  } else {
+
+  }
+}
+
+function tagName(c) {
+  if(c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if(c === '/') {
+    return selfClosingStartTag;
+  } else if(c.match(/^[A-Z]$/)) {
+    currentToken.tagName += c; // toLowerCase()
+    return tagName;
+  } else if (c === '>') {
+    emit(currentToken);
+    return data;
+  } else {
+    currentToken.tagName += c;
+    return tagName;
+  }
+}
+
+function beforeAttributeName(c) {
+  if(c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c === '/' || c === '>' || c === EOF) {
+    return afterAttributeName(c);
+  } else if(c === '=') {
+  } else {
+    currentAttribute = {
+      name: '',
+      value: ''
+    }
+    return attributeName(c);
+  }
+}
+
+function afterAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return afterAttributeName;
+  } else if(c === "/") {
+    return selfClosingStartTag;
+  } else if (c === "=") {
+    return beforeAttributeValue;
+  } else if (c === ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === EOF) {
+
+  } else {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    currentAttribute = {
+      name: "",
+      value: ""
+    };
+    return attributeName(c);
+  }
+}
+
+function selfClosingStartTag(c) {
+  if(c === '>') {
+    currentToken.isSelfClosing = true;
+    emit(currentToken);
+    return data;
+  } else if(c === 'EOF') {
+
+  } else {
+
+  }
+}
+
+function attributeName(c) {
+  if(c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+    return afterAttributeName(c);
+  } else if(c === '=') {
+    return beforeAttributeValue;
+  } else if (c === '\u0000') {
+
+  } else if (c === '\"' || c === "'" || c === '<') {
+
+  } else {
+    currentAttribute.name += c;
+    return attributeName;
+  }
+}
+
+function beforeAttributeValue(c) {
+  if(c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+    return beforeAttributeValue;
+  } else if (c === '\"') {
+    return doubleQuotedAttributeValue;
+  } else if (c === '\'') {
+    return singleQuoteAttributeValue;
+  } else if (c === '>') {
+    return data;
+  } else {
+    return UnquotedAttributeValue(c);
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c === "\"") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c === "\u0000") {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function singleQuoteAttributeValue(c) {
+  if (c === "\'") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c === "\u0000") {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function afterQuotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c === '/') {
+    return selfClosingStartTag;
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function UnquotedAttributeValue(c) {
+  if(c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return beforeAttributeName;
+  } else if (c === '/') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === '\u0000') {
+
+  } else if (c === '\"' || c === "'" || c === '<' || c === '=' || c === "`") {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c;
+    return UnquotedAttributeValue;
+  }
+}
+
+module.exports.parseHTML = function parseHTML(html) {
+  let state = data;
+  for (let c of html) {
+    state = state(c);
+  }
+  state = state(EOF);
+  return state[0];
+}
