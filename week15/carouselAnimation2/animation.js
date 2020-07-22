68c963f47ae0a0dbcb92044eeadf2d598ca8b161
于鑫 <xin.yu@chaitin.com>
2020-07-22 14:55:32 +0800

    feat(week15): add another carousel animation way

---
+++ week15/carouselAnimation2/animation.js
@@ -0,0 +1,163 @@
+// 由于css的动画不可控，比如把
+export class Timeline {
+  constructor() {
+    this.animations = [];
+    this.requestID = null;
+    this.INITED = 0;
+    this.PLAYING = 1;
+    this.PAUSE = 2;
+    this.FINISHED = 3;
+    this.state = this.INITED;
+    // 写在这个地方就可以用箭头函数，写在class里面this是不固定的
+    this.tick = () => {
+      let t = Date.now() - this.startTime;
+      let animations = this.animations.filter(animation => !animation.finished)
+      for (const animation of this.animations) {
+        let { object, property, timingFunction, delay, duration, template, addTime } = animation;
+
+        let progression = timingFunction((t - delay - addTime) / duration); // 0-1之前的数
+        if (t > duration + delay + addTime) {
+          progression = 1;
+        }
+
+        if (!animation.finished) {
+          let value = animation.valueFromProgression(progression);
+
+          object[property] = template(value);
+        }
+
+        if (progression === 1) {
+          animation.finished = true;
+        }
+      }
+      if (animations.length) {
+        this.requestID = requestAnimationFrame(this.tick);
+      } else {
+        this.state = this.INITED;
+        this.animations.forEach(animation => animation.finished = false);
+      }
+    }
+  }
+
+  pause() {
+    if (this.state !== this.PLAYING) {
+      return;
+    }
+    this.state = this.PAUSE;
+    this.pauseTime = Date.now();
+    if (this.requestID !== null) {
+      cancelAnimationFrame(this.requestID);
+    }
+  }
+
+  clear() {
+    this.state = this.INITED;
+    this.animations.length = 0;
+  }
+
+  resume() {
+    if (this.state !== this.PAUSE) {
+      return;
+    }
+    this.state = this.PLAYING;
+    // 为啥是这个？
+    this.startTime += Date.now() - this.pauseTime;
+    this.tick();
+  }
+
+  start() {
+    if (this.state !== this.INITED) {
+      return;
+    }
+    this.state = this.PLAYING;
+    this.startTime = Date.now();
+    this.tick();
+  }
+
+  restart() {
+    if (this.state === this.PLAYING) {
+      this.pause();
+    }
+    this.animations = [];
+
+    this.requestID = null;
+    this.state = this.PLAYING;
+    this.startTime = Date.now();
+    this.pauseTime = null;
+    this.tick();
+  }
+
+  add(animation, addTime) {
+    this.animations.push(animation);
+    animation.finished = false;
+    if (this.state === this.PLAYING) {
+      animation.addTime = addTime !== void 0 ? addTime : Date.now() - this.startTime;
+    } else {
+      animation.addTime = addTime !== void 0 ? addTime : 0;
+    }
+  }
+}
+
+export class Animation {
+  constructor(object, property, start, end, duration, delay, timingFunction, template) {
+    this.object = object;
+    this.template = template;
+    this.property = property;
+    this.start = start;
+    this.end = end;
+    this.duration = duration;
+    this.delay = delay || 0;
+    // 对应ease linear什么的
+    this.timingFunction = timingFunction;
+  }
+
+  valueFromProgression(progression) {
+    return this.start + progression * (this.end - this.start);
+  }
+}
+
+export class ColorAnimation {
+  constructor(object, property, start, end, duration, delay, timingFunction, template) {
+    this.object = object;
+    this.template = template || ((v) => `rgba(${v.r}, ${v.g}, ${v.b}, ${v.a})`);
+    this.property = property;
+    this.start = start;
+    this.end = end;
+    this.duration = duration;
+    this.delay = delay || 0;
+    // 对应ease linear什么的
+    this.timingFunction = timingFunction;
+  }
+
+  valueFromProgression(progression) {
+    return {
+      r: this.start.r + progression * (this.end.r - this.start.r),
+      g: this.start.g + progression * (this.end.g - this.start.g),
+      b: this.start.b + progression * (this.end.b - this.start.b),
+      a: this.start.a + progression * (this.end.a - this.start.a),
+    }
+  }
+}
+
+
+/*
+
+let animation = new Animation(object, property, start, end, duration, delay, duration, timingFunction)
+
+animation.start()
+animation.stop()
+animation.pause()
+animation.resume()
+// 如果有两个动画，怎么stop
+性能的原因，所以才会有时间线
+
+let timeline = new Timeline;
+
+timeline.add(animation);
+timeline.add(animation2);
+
+timeline.start()
+timeline.stop()
+
+
+*/
---
+++ week15/carouselAnimation2/main.js
@@ -0,0 +1,97 @@
+import { createElement, Text, Wrapper } from './createElement';
+import { Timeline, Animation, ColorAnimation } from './animation.js';
+import { cubicBezier } from './cubicBezier.js';
+
+class Carousel {
+  constructor(config) {
+    this.children = [];
+    this.attributes = new Map();
+    this.properties = new Map();
+  }
+  setAttribute(name, value) {
+    this[name] = value;
+  }
+  appendChild(child) {
+    this.children.push(child);
+  }
+  mountTo(parent) {
+    this.render().mountTo(parent);
+  }
+  render() {
+    // JSX的组件是一等公民，可以随便的赋值给变量，这是非常舒服的地方
+    let children = this.data.map(url => {
+      let element = <img src={url} />;
+      element.addEventListener('dragstart', event => event.preventDefault());
+      return element;
+    });
+    let stopBtn = <button>stop</button>
+    let resumeBtn = <button>resume</button>
+    let restartBtn = <button>restart</button>
+    let isStop = false;
+    let root = <div class="container">
+      <div class="carousel">
+        {children}
+      </div>
+      <div class="btn-container">
+        {stopBtn}
+        {resumeBtn}
+        {restartBtn}
+      </div>
+    </div>
+    let position = 0;
+    let tl = new Timeline;
+    let ease = cubicBezier(0.25, .1, .25 ,1);
+    children.forEach((child, index) => {
+      tl.add(new Animation(child.style, 'transform', 0, 100, 1000, 0, ease, v => {
+        return `translateX(${ - v - 100 * (((index === 0) && (position === this.data.length - 1)) ? -1 : position)}%)`
+      }));
+    })
+    let nextPic = () => {
+      tl.start();
+      // 这里加setTimeout是因为transition生效是需要间隔的
+      setTimeout(() => {
+        if (isStop) {
+          return;
+        }
+        position = (position + 1) % this.data.length;
+        nextPic();
+      }, 3000);
+    }
+    // 下面几个功能还不太好用
+    stopBtn.addEventListener('click', () => {
+      if (tl.state === tl.PLAYING) {
+        tl.pause()
+      } else {
+        isStop = true;
+      }
+    });
+    resumeBtn.addEventListener('click', () => {
+      if (isStop) {
+        isStop = false;
+        setTimeout(nextPic, 16);
+      } else {
+        tl.resume();
+      }
+    })
+    restartBtn.addEventListener('click', () => {
+      isStop = false;
+      tl.clear();
+      children.forEach(child => {
+        child.style.transform = '';
+      })
+      position = 0;
+      setTimeout(nextPic, 16);
+    })
+    setTimeout(nextPic, 3000);
+    return root
+  }
+}
+
+let component = <Carousel data={[
+  "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+  "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+  "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+  "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+]}/>
+
+component.mountTo(document.body);
