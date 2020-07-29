9d64eaedceb93681bcd0e6e6c8632c70995b04b4
yuxin <yx18513839223@gmail.com>
2020-07-29 21:51:50 +0800

    feat(week16): add gesture to carousel

--- week16/carouselAnimationGesture3/carousel.js
+++ week16/carouselAnimationGesture3/carousel.js
@@ -24,6 +24,40 @@ export class Carousel {
   render() {
     // JSX的组件是一等公民，可以随便的赋值给变量，这是非常舒服的地方
     let nextPicStopHandler;
+    let position = 0;
+    let nextPic = () => {
+      let nextPosition = (position + 1) % this.data.length;
+
+      let current = children[position];
+      let next = children[nextPosition];
+
+      let currentAnimation = new Animation(
+        current.style,
+        "transform",
+        - 100 * position,
+        -100 - 100 * position,
+        5000,
+        0,
+        ease,
+        v => {
+          return `translateX(${5 * v}px)`
+        }
+      )
+      let nextAnimation = new Animation(
+        next.style,
+        "transform",
+        100 - 100 * nextPosition,
+        - 100 * nextPosition,
+        5000,
+        0,
+        ease,
+        v => `translateX(${5 * v}px)`
+      )
+      timeline.add(currentAnimation);
+      timeline.add(nextAnimation);
+      position = nextPosition;
+      nextPicStopHandler = setTimeout(nextPic, 3000);
+    }
     let children = this.data.map((url, currentPosition) => {
       let lastPosition = (currentPosition - 1 + this.data.length) % this.data.length;
       let nextPosition = (currentPosition + 1) % this.data.length;
@@ -61,45 +95,39 @@ export class Carousel {
         }
         timeline.reset();
         timeline.start();
-        let currentTransformValue = -500 * currentPosition + offset + dx;
-        let lastTransformValue = - 500
-        if (direction) {
-          let currentAnimation = new Animation(
-            current.style,
-            "transform",
-            - 100 * position,
-            -100 - 100 * position,
-            500,
-            0,
-            ease,
-            v => {
-              return `translateX(${v}%)`
-            }
-          )
-          let nextAnimation = new Animation(
-            next.style,
-            "transform",
-            100 - 100 * nextPosition,
-            - 100 * nextPosition,
-            500,
-            0,
-            ease,
-            v => `translateX(${v}%)`
-          )
-          
-        }
+
+        let lastElement = children[lastPosition];
+        let currentElement = children[currentPosition];
+        let nextElement = children[nextPosition];
+
+        let lastAnimation = new Animation(lastElement.style, 'transform',
+          - 500 - 500 * lastPosition + offset + dx, - 500 - 500 * lastPosition + direction * 500, 500, 0, ease, v => `translateX(${v}px)`)
+        let currentAnimation = new Animation(currentElement.style, 'transform',
+          - 500 * currentPosition + offset + dx, - 500 * currentPosition + direction * 500, 500, 0, ease, v => `translateX(${v}px)`)
+
+        let nextAnimation = new Animation(nextElement.style, 'transform',
+          500 - 500 * nextPosition + offset + dx, 500 - 500 * nextPosition + direction * 500, 500, 0, ease, v => `translateX(${v}px)`)
+
+        timeline.add(lastAnimation);
+        timeline.add(currentAnimation);
+        timeline.add(nextAnimation);
+
+        position = (position - direction + this.data.length) % this.data.length;
+
+        nextPicStopHandler = setTimeout(nextPic, 3000);
       }
 
       let element = <img
         src={url}
         onStart={onStart}
         onPan={onPan}
+        onPanend={onPanend}
         enableGesture={true}
       />;
+      element.style.transform = "translateX(0px)";
       element.addEventListener('dragstart', event => event.preventDefault());
       return element;
     });
-    let position = 0;
     let timer = null;
     let timeline = new Timeline;
     let ease = cubicBezier(0.25, .1, .25 ,1);
@@ -121,39 +149,6 @@ export class Carousel {
       </div>
     </div>
     timeline.start();
-    let nextPic = () => {
-      let nextPosition = (position + 1) % this.data.length;
-
-      let current = children[position];
-      let next = children[nextPosition];
-
-      let currentAnimation = new Animation(
-        current.style,
-        "transform",
-        - 100 * position,
-        -100 - 100 * position,
-        5000,
-        0,
-        ease,
-        v => {
-          return `translateX(${5 * v}px)`
-        }
-      )
-      let nextAnimation = new Animation(
-        next.style,
-        "transform",
-        100 - 100 * nextPosition,
-        - 100 * nextPosition,
-        5000,
-        0,
-        ease,
-        v => `translateX(${5 * v}px)`
-      )
-      timeline.add(currentAnimation);
-      timeline.add(nextAnimation);
-      position = nextPosition;
-      nextPicStopHandler = setTimeout(nextPic, 3000);
-    }
     nextPicStopHandler = setTimeout(nextPic, 3000);
     return root
   }
